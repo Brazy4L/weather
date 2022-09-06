8c47918c2543d024fccc7131f59f20a82626efb3
Brazy <me@brazy.one>
2022-09-06 16:47:19 +0300

    Change weekly style

--- src/index.js
+++ src/index.js
@@ -24,56 +24,91 @@ const Run = (() => {
         }
       );
       let geo = await response.json();
-      fetchWeather(geo.results[0].latitude, geo.results[0].longitude);
+      fetchWeeklyWeather(geo.results[0].latitude, geo.results[0].longitude);
+      fetchHourlyWeather(geo.results[0].latitude, geo.results[0].longitude);
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function fetchWeather(lat, long) {
+  async function fetchHourlyWeather(lat, long) {
     try {
       const response = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto`,
+        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,precipitation,weathercode&timezone=auto`,
+        {
+          mode: 'cors',
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchWeeklyWeather(lat, long) {
+    try {
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&daily=sunrise,sunset&daily=weathercode&timezone=auto`,
         {
           mode: "cors",
         }
       );
       const data = await response.json();
-      weather(
+      weeklyWeather(
         data.daily.temperature_2m_max,
         data.daily.temperature_2m_min,
+        data.daily.weathercode,
         data.daily.precipitation_sum,
-        data.daily.time
+        data.daily.time,
+        data.daily.sunrise,
+        data.daily.sunset
       );
     } catch (error) {
       console.log(error);
     }
   }
 
-  const weather = (one, two, three, four) => {
+  const weeklyWeather = (one, two, three, four, five, six, seven) => {
     let hightemp = document.querySelectorAll(".hightemp");
     let lowtemp = document.querySelectorAll(".lowtemp");
+    let icon = document.querySelectorAll(".icon");
     let precip = document.querySelectorAll(".precip");
     let date = document.querySelectorAll(".date");
+    let sunrise = document.getElementById("sunrise");
+    let sunset = document.getElementById("sunset");
 
     for (let i = 0; i < 7; i++) {
       hightemp[i].textContent = one[i] + " °C";
     }
+
     for (let i = 0; i < 7; i++) {
       lowtemp[i].textContent = two[i] + " °C";
     }
+
     for (let i = 0; i < 7; i++) {
-      if (three[i] === null) {
+      icon[i].textContent = three[i];
+    }
+
+    for (let i = 0; i < 7; i++) {
+      if (four[i] === null) {
         precip[i].textContent = "0 mm";
       } else {
-        precip[i].textContent = three[i] + " mm";
+        precip[i].textContent = four[i] + " mm";
       }
     }
+
     for (let i = 0; i < 7; i++) {
-      four[i] = four[i].substring(5);
-      date[i].textContent = four[i];
+      date[i].textContent = five[i].substring(5);
     }
+
+    sunrise.textContent = six[0].substring(11);
+    sunset.textContent = seven[0].substring(11);
   };
+
+  const HourlyWeather = (one, two, three) => {
+
+  }
 
   return {
     app,
